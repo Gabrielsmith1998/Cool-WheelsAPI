9e562690bfd0324998fa38a2dc97c9183ccc14d7
Gabriel Smith PC <damngabe1@gmail.com>
2022-04-11 20:03:41 -0500

    Added Models

---
+++ Cool-WheelsAPI/Models/Track.cs
@@ -0,0 +1,12 @@
+﻿namespace CoolWheels.Models
+{
+    public class Track
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int BuyerId { get; set; }
+        public Buyer Buyer { get; set; }
+    }
+}
