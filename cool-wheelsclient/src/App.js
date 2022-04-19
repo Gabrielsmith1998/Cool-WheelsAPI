255398b7837556f8d89f365066bfa1a30db25d95
albertchitta <albert.w.chittaphong@gmail.com>
2022-04-18 21:03:26 -0500

    test with data api for frontend

--- cool-wheelsclient/src/App.js
+++ cool-wheelsclient/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import getBuyers from './api/data/buyerData';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [buyers, setBuyers] = useState([]);
+
+    useEffect(() => {
+        let isMounted = true;
+        if (isMounted) {
+            getBuyers().then(setBuyers);
+        }
+
+        return () => {
+            isMounted = false;
+        }
+    }, []);
+
+    return (
+        <div className="App">
+            {buyers.length ? (
+                buyers.map((buyer) => (
+                    <h1>Buyer: ${buyer}</h1>
+                ))
+            ) : (
+                <h1>No Buyers</h1>
+            )}
+        </div>
+    );
 }
 
 export default App;
