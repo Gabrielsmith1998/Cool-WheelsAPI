e5e2860f0e1f25510fe41c623f774a401c4df8cf
Klay Thacker <klaythacker11@gmail.com>
2022-04-19 21:06:46 -0500

    Commit

---
+++ cool-wheelsclient/src/Components/CarCards.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+export default function CarCard({ cars }) {
+    /*const handleDelete = (method) => {
+        if (method === 'delete') {
+            deleteStuff(card.Id).then(() => {
+                getCars().then(setCars);
+            });
+        }
+    };*/
+    return (
+        <div class="card" style={{ width: '18rem' }}>
+            <img src={cars.ImageUrl} class="card-img-top" alt="Car" />
+                <div class="card-body">
+                <h5 class="card-title">{cars.Name}</h5>
+                <p class="card-text">{cars.Color}</p>
+                <p class="card-text">{cars.Year}</p>
+                <p class="card-text">{cars.Price}</p>
+                {/*<button
+                    onClick={() => handleDelete('Delete')}
+                    className="btn btn-danger"
+                    type="button"
+                >
+                    Delete
+                </button>*/}
+                </div>
+        </div>
+        );
+};
+
+CarCard.PropTypes = {
+    cars: PropTypes.shape(PropTypes.obj).isRequired,
+};
