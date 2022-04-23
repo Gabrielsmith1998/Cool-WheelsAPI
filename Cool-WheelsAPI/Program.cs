7a659613f89fdd1532711f4d4d6413b9fb417eea
Gabriel Smith PC <damngabe1@gmail.com>
2022-04-23 09:13:47 -0500

    Fixed Cors issue

--- Cool-WheelsAPI/Program.cs
+++ Cool-WheelsAPI/Program.cs
@@ -2,7 +2,17 @@ using Cool_WheelsAPI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://localho.st:3000").AllowAnyOrigin().AllowAnyHeader();
+                      });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,6 +32,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
