57f74209b43fe538613bc3216c2fe39d9d682639
Nathan Blackman <nathanpgblackman@gmail.com>
2022-04-23 12:58:56 -0500

    added navbar and linked it to app.js

--- cool-wheelsclient/src/App.js
+++ cool-wheelsclient/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import React from 'react';
 import PublicRoutes from './Routes';
+import Navigation from './Components/Navbar.js';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
         <div>
+            <Navigation />
             <PublicRoutes />
         </div>
     );
