b09bb540a59cef9483659078473c971cbf77c25d
Nathan Blackman <nathanpgblackman@gmail.com>
2022-04-25 18:36:57 -0500

    safety commit

--- cool-wheelsclient/src/Components/Navbar.js
+++ cool-wheelsclient/src/Components/Navbar.js
@@ -4,26 +4,20 @@ import {
     Navbar,
     Container,
     Nav,
-    NavDropdown,
 } from 'react-bootstrap';
 
 export default function Navigation() {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="../Views/Home.js">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="../Views/Home.js">Cool Wheels</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="../Views/Home.js">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="../Views/Cars.js">Cars</NavDropdown.Item>
-                            <NavDropdown.Item href="../Views/Tracks">Tracks</NavDropdown.Item>
-                            <NavDropdown.Item href="../Views/Profile">Profile</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="../Views/Cars.js">Cars</Nav.Link>
+                        <Nav.Link href="../Views/Tracks.js">Tracks</Nav.Link>
+                        <Nav.Button href="../Views/SignIn.js">Sign In</Nav.Button>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
