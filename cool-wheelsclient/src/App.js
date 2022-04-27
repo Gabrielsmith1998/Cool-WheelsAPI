1c861dbef16abc54042df1eb30b77fa163e6c2f3
Nathan Blackman <nathanpgblackman@gmail.com>
2022-04-26 19:59:25 -0500

    Commented out the SignIn.js link in the navbar since this version didn't have all the pieces yet

--- cool-wheelsclient/src/App.js
+++ cool-wheelsclient/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
-import PublicRoutes from './Routes';
 import Navigation from './Components/Navbar.js';
+import PublicRoutes from './Routes';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
--- cool-wheelsclient/src/Components/Navbar.js
+++ cool-wheelsclient/src/Components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navigation() {
                         <Nav.Link href="../Views/Home.js">Home</Nav.Link>
                         <Nav.Link href="../Views/Cars.js">Cars</Nav.Link>
                         <Nav.Link href="../Views/Tracks.js">Tracks</Nav.Link>
-                        <Nav.Button href="../Views/SignIn.js">Sign In</Nav.Button>
+                        {/*<Nav.Button href="../Views/SignIn.js">Sign In</Nav.Button>*/}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
