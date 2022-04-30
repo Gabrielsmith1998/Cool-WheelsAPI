3682cb6de42321cff834062ddd40dd028b746164
Klay Thacker <klaythacker11@gmail.com>
2022-04-30 14:01:35 -0500

    commit

--- EditTablesBuyers.sql
+++ EditTablesBuyers.sql
@@ -1,8 +1,20 @@
 ﻿--ALTER TABLE Buyer
+--ADD UserName VARCHAR(255);
+--ALTER TABLE Buyer
+--ADD Email VARCHAR(255);
+--ALTER TABLE Buyer
+--ADD About VARCHAR(255);
+--ALTER TABLE Buyer
 --ADD [Image] VARCHAR(255);
 
 --DELETE FROM Buyer
 --                            WHERE Id = 1
+--DELETE FROM Buyer
+--                            WHERE Id = 2
+--DELETE FROM Buyer
+--                            WHERE Id = 3
+--DELETE FROM Buyer
+--                            WHERE Id = 4
 
 --INSERT INTO Buyer ([Name], UserName, Email, About, [Image]) VALUES('Albert Chittaphong', 'albertchitta', 'albert123@gmail.com', 'Albert About Me', 'https://bootdey.com/img/Content/avatar/avatar2.png');
 --INSERT INTO Buyer ([Name], UserName, Email, About, [Image]) VALUES('Klay Thacker', 'KlayTT', 'klay123@gmail.com', 'Klay About Me', 'https://bootdey.com/img/Content/avatar/avatar6.png');
