093312cd0265004625b7d8ad60219c2cd6e67a36
Gabriel Smith PC <damngabe1@gmail.com>
2022-05-09 15:31:49 -0500

    commit

--- cool-wheelsclient/src/Components/TrackCards.js
+++ cool-wheelsclient/src/Components/TrackCards.js
@@ -1,13 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import { deleteTrack } from '../api/data/TrackData';
+import { deleteTrack, getSingleTrack, updateTrack } from '../api/data/TrackData';
 import { Link } from 'react-router-dom';
+import { getAuth, onAuthStateChanged, deleteUser } from "firebase/auth";
+import firebase from 'firebase/compat/app';
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+  };
+  firebase.initializeApp(firebaseConfig);
+  
+  var uid;
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      uid = user.uid;
+    }
+  });
 
 export default function TrackCards({ tracks, setTracks }) {
   const handleDelete = () => {
       deleteTrack(tracks.id).then((track) => setTracks(track));
   };
+
+  const setBuyerId = () => {
+    getSingleTrack(tracks.id).then((track) => updateTrack({...track, buyerId: uid}).then((tracks) => setTracks(tracks)))
+  }
+
+  console.warn(tracks)
     return (
+        <>
         <div className="card">
             <img src={tracks.imageUrl} alt="tracks" />
             <div>
@@ -23,12 +46,20 @@ export default function TrackCards({ tracks, setTracks }) {
                 <button
                     type="button"
                     className="btn btn-danger"
+                    onClick={setBuyerId}
+                >
+                    <i className="far fa-edit" /> Buy
+                </button>
+                <button
+                    type="button"
+                    className="btn btn-danger"
                     onClick={handleDelete}
                 >
                     <i className="far fa-edit" /> Delete
                 </button>
             </div>
         </div>
+        </>
     );
 }
 
